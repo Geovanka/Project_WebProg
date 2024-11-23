5c33dbd5863acee23ba6cb679a6db098f94338e7
Geovanka <geovankakurniawan@gmail.com>
2024-11-23 14:00:10 +0700

    template change

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('presentation');
+    return view('index');
+});
+
+Route::get('/content', function(){
+    return view('content');
+});
+
+Route::get('/system', function(){
+    return view('system');
 });
