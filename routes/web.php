e4154b581da597581a9cf93ed0a39508a1d86871
qendchana <kentchristopherhansel@gmail.com>
2024-11-25 20:37:09 +0700

    logincontroller, registercontroller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -9,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::get('/', function(){
     return view('login');
-});
+})->name('landing');
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
@@ -23,11 +25,15 @@ Route::get('/profile', function(){
     return view('profile');
 });
 
-Route::get('/login', function(){
-    return view('login');
-});
+Route::post('/login', [LoginController::class, 'login'])->name('login');
 
+Route::get('register', [RegisterController::class, 'showRegisterForm'])->name('register');
+Route::post('register', [RegisterController::class, 'register']);
 
 Route::get('/company', function(){
     return view('company');
 });
+
+Route::get('/admin', function(){
+    return view('admin');
+})->name('admin.dashboard');
