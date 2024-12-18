bdf7608a0ecfeb664d2ae0e8d4befca8bfe54f73
Axelle Tanoto <axelle.tanoto@gmail.com>
2024-12-19 02:33:47 +0700

    asd

--- config/database.php
+++ config/database.php
@@ -42,15 +42,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => parse_url(env('DB_URL'))['host'],
-            'port' => parse_url(env('DB_URL'))['port'],
-            'database' => ltrim(parse_url(env('DB_URL'))['path'], '/'),
-            // 'host' => env('DB_HOST', '127.0.0.1'),
-            // 'port' => env('DB_PORT', '3306'),
-            // 'database' => env('DB_DATABASE', 'laravel'),
-            // 'username' => env('DB_USERNAME', 'root'),
-            'username' => parse_url(env('DB_URL'))['user'],
-            'password' => parse_url(env('DB_URL'))['pass'],
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_0900_ai_ci'),
@@ -148,7 +144,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '').'_database'),
         ],
 
         'default' => [
